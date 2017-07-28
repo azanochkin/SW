e7abf3248c09a81ffd35dc0be747fca07faf6cfa
Andrey Zanochkin <andyzanochkin@gmail.com>
2017-07-29 01:59:00 +0400

    New algorithm in convPoint provided

--- code/convPoint.m
+++ code/convPoint.m
@@ -1,20 +1,31 @@
 function h = convPoint( h, method )
+    function f = F(h)
+        [~,f] = getrates( h, convpnt);
+        df = abs(ufr - f);
+        f = log(tau/df);
+    end
     tau = h.method.tau;
     tautol = h.method.tautol;
     convpnt = h.method.convpnt;
     ufr = h.method.ufr;
     llp = max(h.data.u);
-    itcnt = 0;  
-    isnConv = true;
-    while isnConv
-        [~,f] = getrates( h, convpnt);
-        df = abs(ufr - f);
-        isnConv = abs(tau/df - 1) > tautol;
-        if isnConv
+    %
+    a = h.method.alpha;
+    Fa = F(h);
+    h.method.alpha = a - Fa/(convpnt - llp);
+    isnconv = true;
+    itcnt = 1;
+    while isnconv
+        h = method(h);
+        b = h.method.alpha;
+        Fb = F(h);
+        isnconv = abs(Fb) > tautol; %abs(exp(Fb) - 1) > tautol
+        fprintf('conv iter %2i: alpha = %7.5f\n',itcnt,h.method.alpha);
+        if isnconv
+            h.method.alpha = b - (b - a) * Fb/(Fb - Fa);
+            a = b;
+            Fa = Fb;
             itcnt = itcnt + 1;
-            h.method.alpha = h.method.alpha - log(tau/df)/(convpnt - llp);
-            h = method(h);
-            %fprintf('conv iter %2i: alpha = %7.5f\n',itcnt,h.method.alpha);
         end
     end
 end
