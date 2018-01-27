cb37bc1b6e25cbd98cb66667bf0cedea3fa7133b
Andrey Zanochkin <andyzanochkin@gmail.com>
2018-01-27 15:06:34 +0300

    Multiple sigma in error matrix added

--- code/TannerWong/finish.m
+++ code/TannerWong/finish.m
@@ -2,7 +2,7 @@
 load ./data/data_update_may2015.mat
 data.Date = datetime(data.Date,'InputFormat','dd.MM.yyyy');
 %% Set parameters
-Ndt = 10;
+Ndt = 39;
 % dt = data.Date(end-Ndt+1:end);
 dt = data.Date(round(linspace(1,length(data.Date),Ndt)));
 alpha = 0.01;
@@ -15,50 +15,35 @@ for it = 1:Ndt
     arrh(it) = SW(data,dt(it),'functional','new','mask',mask,...
         'norm','simple','alpha',alpha,'UFR',UFR,'kernel',kernel);
     if any(xor(mask,arrh(it).data.mask))
-        warning(it)
+        warning(sprintf('in %i date mask not full',it))
     end
 end
 %
 pTol = 1e-8;
-M = 5e2;
-N = 1e3;
+M = 1e2;
+N = 1e2;
 m = 20;
 n = 20;
 %% Compute
-alpha_x = 4e-2*0.5 * m * Ndt;
-alpha_r = 4e-2*0.5 * n * Ndt;
-beta_x = 4e-2*0.5 * m * Ndt * ones(1,N);%2.1;
-beta_r = 4e-2*0.5 * n * Ndt * ones(1,N);%0.67;
-%
-% beta_x = beta_x * 2e-2;
-% beta_r = beta_r * 1e-2;
+coef = 4e-2;
+alpha_x = coef * 0.5 * m * Ndt;
+alpha_r = coef * 0.5 * Ndt;
+beta_x = coef * 0.5 * m * Ndt * ones(1,N);
+beta_r = coef * 0.5 * Ndt * ones(n,N);
 %%
-% DX = nan(m,N);
-% DR = nan(n,N);
-for l = 1:20
-    fprintf('%3i: sigma^2_x = %6.3f; sigma^2_r = %6.3f\n',l,mean(beta_x)/alpha_x,mean(beta_r)/alpha_r)
-    if true
-        x = linspace(0,3,300);
-        [X,BX] = meshgrid(x,beta_x);
-        pdx = gampdf(X,alpha_x,1./BX);    
-        [X,BR] = meshgrid(x,beta_r);
-        pdr = gampdf(X,alpha_r,1./BR);
-        plot(x,sum(pdx,1)/N,'r',x,sum(pdr,1)/N,'--');
-        pause(0.1)
-    end
-    %
+for l = 1:2000
     tau_x = gamrnd(alpha_x,1./beta_x);
     tau_r = gamrnd(alpha_r,1./beta_r);
     %
     norm_x = zeros(M,N);
-    norm_r = zeros(M,N);
+    norm_r = zeros(n,M,N);
     norm_y = zeros(M,N);
     %
     for it = 1:Ndt
         [m,n,p,U,D,Q0,~,H] = getInitData(arrh(it));
-        S = 1e-3*arrh(it).method.DeltaSq;
-        parfor i = 1:N
-            lambda = tau_x(i)/tau_r(i);
+        S = 1e-1*arrh(it).method.DeltaSq;
+        for i = 1:N
+            lS = tau_x(i)*S./tau_r(:,i);
             dx = zeros(m,1);
             dr = zeros(n,1);
             flag = true;
@@ -68,29 +53,31 @@ for l = 1:20
                 P = diag(U'*D*ex);
                 flag = max(abs(p'-sum(Q))) > pTol;
                 if flag
-                    beta = (Q'*H*Q + lambda*P'*S*P)\(p - Q'*(1 - dx) + P' * dr);
-                    dr = lambda * S * P * beta;
+                    beta = (Q'*H*Q + P'*lS*P)\(p - Q'*(1 - dx) + P' * dr);
+                    dr = lS * P * beta;
                     dx = H * Q * beta;
                 end
             end
             % A = 0.5*(inv(H)-diag(dx)+Q*inv(P*S*P)*Q') + Q*inv(P)*diag(beta)*U'*D*ex;
             % A = inv(A+A');
-            A = H - H*Q*((Q'*H*Q+lambda*P'*S*P)\(Q'*H));
+            A = H - H*Q*((Q'*H*Q+P'*lS*P)\(Q'*H));
             A = 0.5 * (A + A') / tau_x(i);
             L = chol(A,'lower');
 
             Y = randn(m,M);
             X = dx + L*Y;
-            R = (p - Q0'*exp(X))./(U'*D'*exp(X));
+            expX = exp(X);
+            R = (p - Q0'*expX)./((U'*D)*expX);
             norm_x(:,i) = norm_x(:,i) + dot(X,H\X)';
-            norm_r(:,i) = norm_r(:,i) + dot(R,S\R)';
+            norm_r(:,:,i) = norm_r(:,:,i) + R.*(S\R);
             norm_y(:,i) = norm_y(:,i) + dot(Y,Y)';
 
     %         DX(:,i) = dx;%%%!!! prob != 1
     %         DR(:,i) = mean(R,2);%%!!! plob != 1
         end
     end
-    pr = norm_x.* tau_x + norm_r.* tau_r - norm_y;
+    nrt = squeeze(sum(norm_r.* repmat(permute(tau_r,[1 3 2]),1,M)));
+    pr = norm_x.* tau_x + nrt - norm_y;
     pr = exp(-0.5*(pr - mean(pr)));
 %     pr = M * pr./sum(pr);
     cumpr = cumsum(pr);
@@ -100,10 +87,31 @@ for l = 1:20
         i = ind(k);
         j = find(cumpr(:,i) >= rnd(k)*cumpr(end,i),1,'first');
         beta_x(k) = 0 + 0.5 * norm_x(j,i);
-        beta_r(k) = 0 + 0.5 * norm_r(j,i);
+        beta_r(:,k) = 0 + 0.5 * norm_r(:,j,i);
     end
     alpha_x = 0.5 * m * Ndt;
-    alpha_r = 0.5 * n * Ndt;
+    alpha_r = 0.5 * Ndt;
+    %%
+    fprintf('%3i: sigma^2_x = %6.3f; sigma^2_r = %6.3f\n',l,mean(beta_x)/alpha_x,mean(beta_r(:))/alpha_r)
+    if false
+        cla
+        x = linspace(0,3,300);
+%         [X,BX] = meshgrid(x,beta_x);
+%         pdx = gampdf(X,alpha_x,1./BX);    
+%         plot(x,sum(pdx,1)/N,'r-.','DisplayName','tau_x');
+        hold on
+        for ii = 1:n
+            [X,BR] = meshgrid(x,beta_r(ii,:));
+            pdr = gampdf(X,alpha_r,1./BR);
+            fr = sum(pdr,1)/N;
+            [~,maxind] = max(fr);
+            plot(x,fr,'DisplayName',sprintf('\\tau_{%i}',ii));
+            text(x(maxind),fr(maxind)+0.01,sprintf('\\downarrow \\tau_{%i}',ii))
+        end
+        legend show
+        hold off
+        pause(0.1)
+    end
 end
 %%
 % T = 60;
